2559ed8583759513273574dae6c8a32ec9694c56
kunal <kunalgarg054@gmail.com>
2024-05-08 11:37:13 +0530

    feat: frontend initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div className="">
+      <h1>App</h1>
+      <Button>Save</Button>
+    </div>
+  )
+}
+
+export default App
